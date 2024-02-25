ffc044bf248662740f68570b20791092d0efd41d
Tyler Graf <tylergraf@gmail.com>
2024-02-25 12:46:42 -0700

    cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express")
+const cors = require('cors');
 var app = express()
 var db = require("./database.js")
 
 var bodyParser = require("body-parser");
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
